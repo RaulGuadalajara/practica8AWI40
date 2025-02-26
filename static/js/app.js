a817c78ec3d52890665800ec1c5cbbeeece2ae2d
RaulGuadalajara <omargdl0506@gmail.com>
2025-02-26 14:43:02 -0600

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -19,25 +19,13 @@ app.config(function ($routeProvider, $locationProvider) {
         templateUrl: "/app",
         controller: "appCtrl"
     })
-    .when("/productos", {
-        templateUrl: "/productos",
-        controller: "productosCtrl"
+    .when("/decoraciones", {
+        templateUrl: "/decoraciones",
+        controller: "decoracionesCtrl"
     })
-    .when("/alumnos", {
-        templateUrl: "/alumnos",
-        controller: "alumnosCtrl"
-    })
-    .when("/ventas", {
-        templateUrl: "/ventas",
-        controller: "ventasCtrl"
-    })
-    .when("/reportes", {
-        templateUrl: "/reportes",
-        controller: "reportesCtrl"
-    })
-    .when("/notificaciones", {
-        templateUrl: "/notificaciones",
-        controller: "notificacionesCtrl"
+    .when("/paquetes", {
+        templateUrl: "/paquetes",
+        controller: "paquetesCtrl"
     })
     .otherwise({
         redirectTo: "/"
@@ -86,16 +74,10 @@ app.run(["$rootScope", "$location", "$timeout", function($rootScope, $location, 
 app.controller("appCtrl", function ($scope, $http) {
     // alert("Hola, soy el controlador app")
 })
-app.controller("productosCtrl", function ($scope, $http) {
+app.controller("decoracionesCtrl", function ($scope, $http) {
     // alert("Hola, soy el controlador productos")
 })
-app.controller("alumnosCtrl", function ($scope, $http) {
-})
-app.controller("ventasCtrl", function ($scope, $http) {
-})
-app.controller("reportesCtrl", function ($scope, $http) {
-})
-app.controller("notificacionesCtrl", function ($scope, $http) {
+app.controller("paquetesCtrl", function ($scope, $http) {
 })
 
 const DateTime = luxon.DateTime
